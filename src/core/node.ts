a609ee5e32d27f4a91f24bd41b22ce1c56b2bae6
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-09-18 15:17:45 +0900

    2.3 変数

--- src/core/node.ts
+++ src/core/node.ts
@@ -154,12 +154,6 @@ export class LocalsNode {
   }
 }
 
-const Op = {
-  I32Const: 0x41,
-  End: 0x0b,
-} as const
-type Op = typeof Op[keyof typeof Op]
-
 export class ExprNode {
   instrs: InstrNode[] = []
   endOp!: Op
@@ -185,6 +179,14 @@ export class ExprNode {
   }
 }
 
+const Op = {
+  LocalGet: 0x20,
+  LocalSet: 0x21,
+  I32Const: 0x41,
+  End: 0x0b,
+} as const
+type Op = typeof Op[keyof typeof Op]
+
 export class InstrNode {
   opcode: Op
 
@@ -192,6 +194,10 @@ export class InstrNode {
     switch (opcode) {
       case Op.I32Const:
         return new I32ConstInstrNode(opcode)
+      case Op.LocalGet:
+        return new LocalGetInstrNode(opcode)
+      case Op.LocalSet:
+        return new LocalSetInstrNode(opcode)
       default:
         return null
     }
@@ -211,5 +217,21 @@ export class I32ConstInstrNode extends InstrNode {
 
   load(buffer: Buffer) {
     this.num = buffer.readI32()
+  }
+}
+
+export class LocalGetInstrNode extends InstrNode {
+  localIdx!: number
+
+  load(buffer: Buffer) {
+    this.localIdx = buffer.readU32()
+  }
+}
+
+export class LocalSetInstrNode extends InstrNode {
+  localIdx!: number
+
+  load(buffer: Buffer) {
+    this.localIdx = buffer.readU32()
   }
 }
